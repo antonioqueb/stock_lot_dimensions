c1dac615e2f92a406542f240aaa6382979807d9e
Antonio Queb <cardstillo@gmail.com>
2025-11-23 16:06:52 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -30,6 +30,7 @@
         'reports/sale_order_custom_detail_report.xml',
         'reports/sale_order_custom_summary_report.xml',
         'reports/sale_quotation_custom_report.xml',
+        'data/mail_template_summary.xml',
         'views/stock_lot_views.xml',
         'views/stock_lot_group_views.xml',
         'views/stock_move_views.xml',
