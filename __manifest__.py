cd676430f7e54bf19ca7d60fe3f23455fe1745b5
Antonio Queb <cardstillo@gmail.com>
2026-02-18 04:43:59 -0600

    Agregar nuevo reporte de detalle personalizado de orden de venta para mejorar la funcionalidad de informes.

--- __manifest__.py
+++ __manifest__.py
@@ -31,6 +31,7 @@
         'reports/report_delivery_custom_detail.xml',
         'reports/pick_ticket_report.xml',
         'reports/report_sale_order_no_price.xml',
+        'reports/sale_order_custom_detail_report.xml',
         'views/stock_lot_views.xml',
         'views/stock_lot_group_views.xml',
         'views/stock_move_views.xml',
